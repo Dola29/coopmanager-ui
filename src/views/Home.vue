d1b377924e9bb9dfacbcdf0b2fc700a02e3e35a9
daniel.leal <danielosvaldoleal@hotmail.com>
2022-03-29 02:01:16 -0400

    Made the  register, login, and home

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mt-5 text-center">
+    <h3>{{ message }}</h3>
+
+    <a href="javascript:void(0)" class="btn btn-lg btn-primary"
+      @click="logout"
+    >Logout</a>
+  </div>
+</template>
+
+<script>
+import {onMounted, ref} from "vue";
+import axios from "axios";
+import {useRouter} from "vue-router";
+
+export default {
+  name: "Home",
+  setup() {
+    const message = ref('');
+    const router = useRouter();
+
+    onMounted(async () => {
+      try {
+        const {data} = await axios.get('auth/user');
+        message.value = `Hi ${data.name}`;
+      } catch (e) {
+        await router.push('auth/login');
+      }
+    });
+
+    const logout = async () => {
+      await axios.post('auth/logout', {}, {withCredentials: true});
+
+      axios.defaults.headers.common['Authorization'] = '';
+
+      await router.push('/login');
+    }
+
+    return {
+      message,
+      logout
+    }
+  }
+}
+</script>
